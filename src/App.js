7637af78bf5d0cc96c62825e59439fe5aec380e2
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-10-28 12:18:31 +0200

    update app name

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         height: '90vh',
       }}
     >
-      <Title style={{ textAlign: 'center' }}>TODO APP</Title>
+      <Title style={{ textAlign: 'center' }}>TODO LIST</Title>
       <Filters />
       <Divider />
       <TodoList />
